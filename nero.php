7d4df699cc7a686a001cb99789771fd04e9a9c8c
malick95 <malick95@hotmail.it>
2017-12-04 22:44:59 +0100

    nero.php

---
+++ nero.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+
+<head>
+    <meta http-equiv="Content-Type" content="txt/html" ; charset="utf-8">
+    <title>ZingBox</title>
+	
+    <meta name="title" content="ZingBox - Visualizzatore di anime e manga" />
+    <meta name="description" content="Questo sito è stato creato per l'esame di Tecnologie Web dell'Unipd. Questo sito permette la lettura dei famosi fumetti giapponesi chiamati 'manga' e la visualizzazione dei cartoni animati chiamati 'anime'." />
+    <meta name="keywords" content="anime, manga, one piece, naruto, bleach, episodio, capitolo" />
+    <link rel="icon" href="img/logo.png" />
+</head>
+<body>
+<?php 
+$log=$_GET[log];
+if($log=="l"){
+echo " <div style=\"display:block;width:500 ;height:500; border:1px; margin-left:500\">eiskugfksdgyfsgluif</div>";
+}
+?>
+<a href="nero.php?log=l">Apri la popup</a>
+</body>
