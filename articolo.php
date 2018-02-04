35274e3e585f3c9851ed2bd3a24fd928514e5b09
malick95 <malick95@hotmail.it>
2018-02-04 18:02:28 +0100

    Aggiunta js

--- articolo.php
+++ articolo.php
@@ -2,7 +2,6 @@
 		session_start();
 		if($_SESSION['user'])include('headerp.html');
 		else include('header.html');
-		include('logsign.php');
 		include('zoom.php');
 		include('footer.html');
 	?>
