83ff906d528023dfeaa1fba2e6ab00c54303d90e
malick95 <malick95@hotmail.it>
2018-02-04 18:06:09 +0100

    Cambiamenti home.php

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
 	<?php
-		include('header.html');
+		session_start();
+		if($_SESSION['user']){include('headerp.html');}
+		else{	include('header.html');}
 		include('logsign.php');
 		include('vetrina.html');
 		include('ultimih.php');
